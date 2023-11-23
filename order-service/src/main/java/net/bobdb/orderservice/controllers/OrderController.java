1f54b6080c139a8807af4fb46fcdc1b20f1837ce
bobdb <bobdb40@gmail.com>
2023-11-23 01:05:19 -0500

    removed lingering security code and added actuator to Orderservice and setup ciruit breaker. don't have any testing code which would be nice

--- order-service/src/main/java/net/bobdb/orderservice/controllers/OrderController.java
+++ order-service/src/main/java/net/bobdb/orderservice/controllers/OrderController.java
@@ -1,5 +1,6 @@
 package net.bobdb.orderservice.controllers;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import net.bobdb.orderservice.dto.OrderRequest;
 import net.bobdb.orderservice.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallback")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order placed succesfully.";
     }
 
+    public String fallback(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Something went wrong.  Try again";
+    }
 
 }
